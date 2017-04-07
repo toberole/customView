3200d78d5e2dacc76d97e35c0746c852021a5442
zhouwei11 <zhouwei11@58ganji-corp.com>
2017-04-07 14:10:59 +0800

    modify
    Signed-off-by: zhouwei11 <zhouwei11@58ganji-corp.com>

--- app/src/main/java/com/zhouwei/customview/MainActivity.java
+++ app/src/main/java/com/zhouwei/customview/MainActivity.java
@@ -11,11 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 提交一行代码
-        Log.i("AAAA","小哥很坏哦");
-        
-        // AAAAAAAAAAAAAAAAAAAAAA
-        
-        // BBBBBBBBBBBBBBBBBBBBBBBB
+
     }
 }
