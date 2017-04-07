07135be2a8370518f2aac7dbf2730372d442a26c
unknown <zhouwei11@010A1612190043.58ganji-corp.com>
2017-04-07 20:17:14 +0800

    review

--- app/src/main/java/com/zhouwei/customview/test/Student.java
+++ app/src/main/java/com/zhouwei/customview/test/Student.java
@@ -12,34 +12,4 @@ public class Student {
     private String gender;
 
     private String birthday;
-    
-    public int add(int x,int y){
-    	return x+y;	
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getBirthday() {
-        /**
-         * 生日
-         */
-        return birthday;
-    }
-
-
-    /*BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB*/
-
-
-
-    /*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*/
 }
