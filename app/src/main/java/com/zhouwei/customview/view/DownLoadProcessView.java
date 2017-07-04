ae2514990e2722d03ae99eb9868c427007f93638
zhouwei <zhouwei11@58ganji.com>
2017-07-04 11:10:12 +0800

    _downLoadProcessView

---
+++ app/src/main/java/com/zhouwei/customview/view/DownLoadProcessView.java
@@ -0,0 +1,24 @@
+package com.zhouwei.customview.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by zhouwei on 2017/7/4.
+ */
+
+public class DownLoadProcessView extends View {
+    public DownLoadProcessView(Context context) {
+        this(context, null);
+    }
+
+    public DownLoadProcessView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DownLoadProcessView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
